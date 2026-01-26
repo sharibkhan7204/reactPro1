833e1c6e509a4f48d957fcfa915f6ff9963a1680
sharibkhan7204 <sharibkhan7204@gmail.com>
2026-01-27 00:53:23 +0530

    third cummit

--- 01-project/mini-pro-1/src/components/Card.jsx
+++ 01-project/mini-pro-1/src/components/Card.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import {Bookmark} from "lucide-react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-   
     <div className="card">
 
         <div>
           <div className="top">
-            <img src='https://tse1.mm.bing.net/th/id/OIP.dLl9UyA6y1GTydI-npnoygHaHv?pid=Api&P=0&h=180'/>
+            <img src={props.logo}/>
             <button>Save <Bookmark size={14}/></button>
           </div>
 
           <div className="center">
-              <h3>Amazon <span>5 days ago</span></h3>
+              <h3>{props.company} <span>{props.datePosted}</span></h3>
               <h2>Senier UI/UX Designer</h2>
 
               <div className='tag'>
-                <h4>Part Time</h4>
-                <h4>Senier Level</h4>
+                <h4>{props.tag1}</h4>
+                <h4>{props.tag2}</h4>
               </div>
           </div>
 
@@ -26,8 +25,8 @@ const Card = () => {
 
         <div className="bottom">
             <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
             </div>
 
             <button>Apply Now</button>
